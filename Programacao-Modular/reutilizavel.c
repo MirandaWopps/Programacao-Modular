9cf4af8f7eb12722bd14a60d7623f75112b787ef
Miranda Wopps <127881339+MirandaWopps@users.noreply.github.com>
2023-05-26 19:52:18 -0300

    Função abreArq: reutilizavel subindo, mas sua documentação é duvidável.

--- Programacao-Modular/reutilizavel.c
+++ Programacao-Modular/reutilizavel.c
@@ -1,9 +1,24 @@
 #include <stdio.h>
 #include "reutilizavel.h"
-FILE* abreArq(char* arqNome, char* mode) {
-	FILE* arq = fopen(arqNome, mode);
-	if (arq == NULL) {
+
+/* Funcao dedicada a lidar com arquivos com tratamento de erros incluso. */
+
+/* Assertivas de entrada 
+     - Lidar com determinado arquivo e seu modo de abertura.
+   Assertivas de saida 
+     - o programa aborta com saída -1.
+	 - O retorna arquivo aberto no modo escolhido. 
+*/
+
+
+FILE* abreArq(char* arqNome, char* mode) { // Parametro 'arqNome' recebe o nome do arquivo e 'mode' o modo de acesso, isto e, leitura, escrita, append ...
+	FILE* arq = fopen(arqNome, mode); // A variavel arq recebe o arquivo aberto pelo comando fopen conforme os parametros da funcao.
+	if (arq == NULL) { // Se a variavel for NULL a funcao nao conseguiu adquirir tudo que precisava.
 		printf("Erro ao abrir o arquivo %s no modo %s !\n", arqNome, mode);
+		exit(-1); // Arquivo nao encontrado  
 	}
-	return arq;
+	return arq;   // Retorna parametro
 }
+
+
+// Interface com o usuário:  "Erro ao abrir o arquivo %s no modo %s !\n"  NOME DE ARQUIVO, OU MODO, OU PERMISSAO DE EDICAO/LEITURA PROVOCANDO ERRO
