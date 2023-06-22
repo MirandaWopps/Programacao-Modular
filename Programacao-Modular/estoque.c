463e3220c14502115408df607e8dcb76b046c53c
Luiz <luizfelipedasilvaseibel@gmail.com>
2023-06-21 23:36:41 -0300

    1) Alteracao no estoque: - Cria uma lista encadeada a partir de um json - Gera um arquivo json a partir de uma lista encadeada
    2) Uso da biblioteca cJSON (Vou ter que dar commit nela tmb)

--- Programacao-Modular/estoque.c
+++ Programacao-Modular/estoque.c
@@ -2,58 +2,157 @@
 #include <stdlib.h>
 #include <string.h>
 #include "estoque.h"
+#include "cJSON.h"
 //#include "reutilizavel.h"
 
-// Funcao auxiliar de criaNo, e usada para preencher a struct Jogo dado
-Jogo preencheDado(char* nome, int codigo, int qntd){
-	Jogo novoDado;
-	strcpy(novoDado.nome, nome);
-	novoDado.codigo = codigo;
-	novoDado.disponibilidade = qntd;
+/*
+	Compilacao: gcc -o "nome_executavel" "nome_arquivo.c" cJSON/cJSON.c -IcJSON -LcJSON -lcjson
+	
+	Coisas que faltam:
+		- Criar um editor de no da lista encadeada, onde passo codigo e ele pergunta o que tem que ser editado
+*/
 
-	return novoDado;
+// Funcao auxiliar para criar um novo nó
+Estoque* criaNo(Jogo dado) {
+    Estoque* novo = (Estoque*)malloc(sizeof(Estoque));
+    novo->dados = dado;
+    novo->prox = NULL;
+
+    return novo;
 }
 
-// Funcao para criar um novo no
-Estoque* criaNo(Jogo dado){
-	Estoque* novo = (Estoque*) malloc(sizeof(Estoque));
-	novo->dados = dado;
-	novo->prox = NULL;
-
-	return novo;
+// Insere um nó no início da lista
+void insereNo(Estoque** lista, Jogo dado) {
+    Estoque* novoNo = criaNo(dado);
+    novoNo->prox = *lista;
+    *lista = novoNo;
 }
 
-// Insere um no no inicio da lista
-void insereNo(Estoque** lista, Jogo dado){
-	Estoque* novoNo = criaNo(dado);
-	novoNo->prox = *lista;
-	*lista = novoNo;
+// Lê o arquivo JSON e cria a lista encadeada 
+Estoque* leArquivoJSON(char* nomeArquivo) {
+	// ========= Tratamento do arquivo =========
+
+	// Realiza a abertura do arquivo
+    FILE* arquivo = fopen(nomeArquivo, "r");
+    if (arquivo == NULL) {
+        printf("Erro ao abrir o arquivo.\n");
+        return NULL;
+    }
+
+	// Pega o tamanho do arquivo indo com o ponteiro ate o final usando seek
+    fseek(arquivo, 0, SEEK_END);
+    long tamanhoArquivo = ftell(arquivo);
+	// Volta o ponteiro para o inicio do arquivo
+    fseek(arquivo, 0, SEEK_SET);
+
+	// Realiza um malloc para conteudoArquivo, onde vai ser salvo o arquivo todo json
+    char* conteudoArquivo = (char*)malloc(tamanhoArquivo + 1);
+	// Faz a leitura o arquivo Json e salva em conteudoJson
+    fread(conteudoArquivo, 1, tamanhoArquivo, arquivo);
+	// Coloca um '\0' para finalizar a string
+    conteudoArquivo[tamanhoArquivo] = '\0';
+
+    fclose(arquivo);
+
+	// ========= Inclusao na lista encadeada =========
+
+	// Retorna a estrutura de dados do Json e atribui em listaJson
+    cJSON* listaJson = cJSON_Parse(conteudoArquivo);
+    if (listaJson == NULL) {
+        printf("Erro ao fazer o parsing do JSON.\n");
+        free(conteudoArquivo);
+        return NULL;
+    }
+
+    Estoque* lista = NULL;
+    cJSON* noJson = NULL;
+
+    cJSON_ArrayForEach(noJson, listaJson) {
+		// Pega os elementos para serem adicionados e coloca em um auxiliar
+        cJSON* nomeJson = cJSON_GetObjectItem(noJson, "nome");
+        cJSON* codigoJson = cJSON_GetObjectItem(noJson, "codigo");
+        cJSON* dispJson = cJSON_GetObjectItem(noJson, "disponibilidade");
+
+        if (nomeJson && codigoJson && dispJson) {
+			// atribui os elementos na struct jogo
+            Jogo jogo;
+			strcpy(jogo.nome, nomeJson->valuestring);
+            jogo.codigo = codigoJson->valueint;
+            jogo.disponibilidade = dispJson->valueint;
+
+			// adiciona na lista encadeada, passando a struct preenchida
+            insereNo(&lista, jogo);
+        }
+    }
+
+	// deleta o json e da free no arquivo onde tem todo o texto json
+    cJSON_Delete(listaJson);
+    free(conteudoArquivo);
+
+    return lista;
 }
 
 // Imprime o estoque
-void imprimeEstoque(Estoque* lista){
-	Estoque* aux = lista;
-	printf("==== Estoque ====\n");
-	while (aux != NULL) {
+void imprimeEstoque(Estoque* lista) {
+    Estoque* aux = lista;
+    printf("==== Estoque ====\n");
+    while (aux != NULL) {
         printf("Codigo: %d - Nome: %s - Disponibilidade: %d\n", aux->dados.codigo, aux->dados.nome, aux->dados.disponibilidade);
         aux = aux->prox;
     }
 }
 
-int main(){
-	Estoque* lista = NULL;
-	Jogo dado;
-	
-	dado = preencheDado("Teste", 1, 2);
-	insereNo(&lista, dado);
-	dado = preencheDado("Teste2", 2, 0);
-	insereNo(&lista, dado);
-	imprimeEstoque(lista);
+// Cria um arquivo json a partir de uma lista enca
+void geraJson(char* texto, const char* caminho) {
+    FILE* arq = fopen(caminho, "w");
+    if (arq == NULL) {
+        printf("Erro ao criar o arquivo.\n");
+        return;
+    }
 
+    fputs(texto, arq);
+    fclose(arq);
+
+    printf("Arquivo JSON criado com sucesso: %s\n", caminho);
+}
+
+// Do estoque cria um arquivo em json
+cJSON* estoqueJson(Estoque* lista) {
+    cJSON* listaJson = cJSON_CreateArray();
+    Estoque* aux = lista;
+
+    while (aux != NULL) {
+        cJSON* noJson = cJSON_CreateObject();
+        cJSON_AddStringToObject(noJson, "nome", aux->dados.nome);
+        cJSON_AddNumberToObject(noJson, "codigo", aux->dados.codigo);
+        cJSON_AddNumberToObject(noJson, "disponibilidade", aux->dados.disponibilidade);
+        cJSON_AddItemToArray(listaJson, noJson);
+        aux = aux->prox;
+    }
+
+    return listaJson;
+}
+
+int main(){
+
+	char* nomeArquivo = "estoque.json";
+    
+
+	Estoque* lista = leArquivoJSON(nomeArquivo);
+
+	printf("Bem vindo a Locadora !\n"); // Introducao
+	imprimeEstoque(lista);
+	
+
+
+	//Bloco para finalizar o programa e salvar o estoque
+	cJSON* dadoJson = estoqueJson(lista);
+    char* textoJson = cJSON_Print(dadoJson);
+    //printf("JSON: %s\n", textoJson);
+	geraJson(textoJson, "estoque.json");
 
 	return 0;
 }
-
 
 // Nome/Objetivo: void controlaAluguel
 // Requsitos: Receber 
--- Programacao-Modular/estoque.h
+++ Programacao-Modular/estoque.h
@@ -8,3 +8,8 @@ struct estoque{
     Jogo dados;
     struct estoque* prox;
 };typedef struct estoque Estoque;
+
+Jogo preencheDado(char* nome, int codigo, int qntd);
+Estoque* criaNo(Jogo dado);
+void insereNo(Estoque** lista, Jogo dado);
+void imprimeEstoque(Estoque* lista);
